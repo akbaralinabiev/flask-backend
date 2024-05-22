363f776639dc65b6ab5acd69235b18f2981405e3
akbaralinabiev <akbaralinabiev14@gmail.com>
2024-05-22 22:00:59 +0200

    files configuration

--- backend.py
+++ backend.py
@@ -1,83 +1,3 @@
-# from flask import Flask, request, jsonify
-# from werkzeug.utils import secure_filename
-# import os
-# from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
-# import torch
-# from PIL import Image
-# from flask_cors import CORS
-
-# # Initialize the Flask application
-# app = Flask(__name__)
-# CORS(app)
-
-# # Configure the upload folder
-# UPLOAD_FOLDER = './uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# # Create the upload folder if it doesn't exist
-# if not os.path.exists(UPLOAD_FOLDER):
-#     os.makedirs(UPLOAD_FOLDER)
-
-# # Load the pre-trained model and tokenizer
-# model = VisionEncoderDecoderModel.from_pretrained(
-#     "nlpconnect/vit-gpt2-image-captioning")
-# feature_extractor = ViTImageProcessor.from_pretrained(
-#     "nlpconnect/vit-gpt2-image-captioning")
-# tokenizer = AutoTokenizer.from_pretrained(
-#     "nlpconnect/vit-gpt2-image-captioning")
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model.to(device)
-
-# max_length = 30
-# num_beams = 10
-# gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-
-
-# def predict_caption(image_path):
-#     i_image = Image.open(image_path)
-#     if i_image.mode != "RGB":
-#         i_image = i_image.convert(mode="RGB")
-
-#     pixel_values = feature_extractor(
-#         images=[i_image], return_tensors="pt").pixel_values
-#     pixel_values = pixel_values.to(device)
-
-#     output_ids = model.generate(pixel_values, **gen_kwargs)
-
-#     preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-#     print(f"Generated caption: {preds[0]}")  # Debugging line
-#     return preds[0]
-
-
-# @app.route('/')
-# def index():
-#     return "Image Captioning API"
-
-
-# @app.route('/caption', methods=['POST'])
-# def caption():
-#     if 'image' not in request.files:
-#         return jsonify({'error': 'No image part'}), 400
-#     file = request.files['image']
-#     if file.filename == '':
-#         return jsonify({'error': 'No selected file'}), 400
-#     if file:
-#         filename = secure_filename(file.filename)
-#         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#         file.save(file_path)
-
-#         print(f"Image saved to {file_path}") 
-
-#         caption = predict_caption(file_path)
-#         print(f"Caption sent to client: {caption}")
-#         return jsonify({'caption': caption})
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import os
@@ -85,9 +5,6 @@ from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoToken
 import torch
 from PIL import Image
 from flask_cors import CORS
-
-# Add import statement at the top of your script
-import os
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -150,15 +67,13 @@ def caption():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
 
-        print(f"Image saved to {file_path}")
+        print(f"Image saved to {file_path}") 
 
         caption = predict_caption(file_path)
         print(f"Caption sent to client: {caption}")
         return jsonify({'caption': caption})
 
 
-# Modify the app.run() call at the bottom of your script
 if __name__ == '__main__':
-    # Use the port provided by Heroku or default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
+
